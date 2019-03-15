89d4d066af4507ee3a0f420203bac28b835af8e3
zorin <boolentfv@gmail.com>
2019-03-15 21:09:25 +0400

    Добавлено получение задания

---
+++ engine/static/tan_engine/js/code.js
@@ -0,0 +1,31 @@
+function CodeEditor(id){
+	let self = this;
+	let editor = ace.edit(id+"-editor");
+	self.editor = editor;
+	self.name = $("."+id+"-name");
+	self.ex= $("."+id+"-ex");
+	editor.setTheme("ace/theme/monokai");
+	editor.getSession().setMode("ace/mode/python");
+	self.bindSkip = function(event, calback){
+		$(id+'-skip').bind(event, calback);
+	}
+	self.bindSend = function(event, calback){
+		$(id+'-send').bind(event, calback);
+	}
+	self.getCode=function(){
+		return editor.getValue();
+	}
+	self.setCode=function(code){
+		return editor.setValue(code);
+	}
+	self.show(){
+		$('.'+id).modal('show');
+	}
+	self.close(){
+		$('.'+id).modal('close');
+	}
+}
+/*
+* Сюда обращатся для работы с эдитором
+*/
+var editor = new CodeEditor('code');
