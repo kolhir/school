4010c753888e78ee7057b2bfbad0059c0ae22cfb
zorin <boolentfv@gmail.com>
2019-03-16 10:36:04 +0400

    Добавлена отправка решения в модальное окно.

--- engine/static/tan_engine/js/code.js
+++ engine/static/tan_engine/js/code.js
@@ -2,15 +2,17 @@ function CodeEditor(id){
 	let self = this;
 	let editor = ace.edit(id+"-editor");
 	self.editor = editor;
-	self.name = $("."+id+"-name");
-	self.ex= $("."+id+"-ex");
+	self.name = $("#"+id+"-name");
+	self.ex= $("#"+id+"-ex");
+	results= $("#"+id+"-results");
+	sendBtn = $('#'+id+"-results").bind('click',_sendCode);
 	editor.setTheme("ace/theme/monokai");
 	editor.getSession().setMode("ace/mode/python");
 	self.bindSkip = function(event, calback){
-		$(id+'-skip').bind(event, calback);
+		$('#'+id+'-skip').bind(event, calback);
 	}
 	self.bindSend = function(event, calback){
-		$(id+'-send').bind(event, calback);
+		$('#'+id+'-send').bind(event, calback);
 	}
 	self.getCode=function(){
 		return editor.getValue();
@@ -19,12 +21,39 @@ function CodeEditor(id){
 		return editor.setValue(code);
 	}
 	self.show = function(){
-		$('.'+id).modal('show');
+		$('#'+id).modal('show');
 	}
 	self.close = function(){
-		$('.'+id).modal('close');
+		$('#'+id).modal('close');
+	}
+	/*
+		Отправка запроса для проверки кода.
+	*/
+	function _sendCode(){
+		data = {"command":"code","id":self.getCode()};
+		$.ajax({
+			method:'POST',
+			dataType: 'json',
+			data:data,
+			url: 'http://127.0.0.1:5000/game/command/',
+			success: function(data){
+				if(data[status]=="success"){
+					results.html(
+						'<h4>Задача решена успешно, можете закрыть окно</h4>'
+					);
+				}else{
+					results.html(
+						'<h4>Ошибка!</h4><code>'.error.'</code>'
+					);
+				}
+			},
+	 		error:function(data){
+				results.text("Ошибка соединения!");
+			}
+		});
 	}
 }
+
 /*
 * Сюда обращатся для работы с эдитором
 */
